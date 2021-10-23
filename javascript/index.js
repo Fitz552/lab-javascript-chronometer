ae5d6f970e5373bab1de215e6f6fa69762e3c18c
lucas552 <lgstehling@gmail.com>
2021-10-23 20:16:26 -0300

    iteration 2 done

--- javascript/index.js
+++ javascript/index.js
@@ -14,16 +14,23 @@ const milUniElement = document.getElementById('milUni');
 const splitsElement = document.getElementById('splits');
 
 function printTime() {
-  // ... your code goes here
+  printMinutes();
+  printSeconds();
+  console.log(chronometer.currentTime)
 }
 
 function printMinutes() {
-  // ... your code goes here
+  let minutes = chronometer.computeTwoDigitNumber(chronometer.getMinutes())
+  minDecElement.innerHTML=minutes[0];
+  minUniElement.innerHTML=minutes[1];
 }
 
 function printSeconds() {
-  // ... your code goes here
+  let seconds = chronometer.computeTwoDigitNumber(chronometer.getSeconds())
+  secDecElement.innerHTML=seconds[0];
+  secUniElement.innerHTML=seconds[1];
 }
+
 
 // ==> BONUS
 function printMilliseconds() {
@@ -39,27 +46,43 @@ function clearSplits() {
 }
 
 function setStopBtn() {
-  // ... your code goes here
+  btnLeftElement.setAttribute("class", "btn stop");
+  btnLeftElement.innerHTML= "STOP";
 }
 
 function setSplitBtn() {
-  // ... your code goes here
+  btnRightElement.setAttribute("class", "btn split");
+  btnRightElement.innerHTML= "SPLIT";
 }
 
 function setStartBtn() {
-  // ... your code goes here
+  btnLeftElement.setAttribute("class", "btn start");
+  btnLeftElement.innerHTML= "START";
 }
 
 function setResetBtn() {
-  // ... your code goes here
+  btnRightElement.setAttribute("class", "btn reset");
+  btnRightElement.innerHTML= "RESET";
 }
 
 // Start/Stop Button
 btnLeftElement.addEventListener('click', () => {
-  // ... your code goes here
+  if(btnLeftElement.className.includes("start")) {   
+    setStopBtn();
+    setSplitBtn();
+    chronometer.start();
+  }
+  else {
+    setResetBtn();
+    setStartBtn();
+    console.log(chronometer.currentTime)
+    chronometer.stop();
+  }
 });
 
 // Reset/Split Button
 btnRightElement.addEventListener('click', () => {
   // ... your code goes here
 });
+
+let Id = setInterval(()=> {printTime()},1)
